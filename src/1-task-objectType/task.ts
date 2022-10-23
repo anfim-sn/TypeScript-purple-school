86e3a085a50becb20dbe17f907e5d76d3aaedecd
Snegirev Anfim <anfim.sn@gmail.com>
2022-10-23 15:22:18 +0400

    Add 1 task

---
+++ src/1-task-objectType/task.ts
@@ -0,0 +1,21 @@
+const data: {
+  officeId: number,
+  isOpened: boolean,
+  contacts: {
+    phone: string,
+    email: string,
+    address: {
+      city : string
+    }
+  }
+} = {
+  officeId: 45,
+  isOpened: false,
+  contacts: {
+    phone: '+79100000000',
+    email: 'office@gmail.com',
+    address: {
+      city : 'San-Francisco'
+    }
+  }
+}
